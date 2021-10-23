7eaef7c213972e4d29e76ffe8204c76edc672239
Sahanon Phisetpakasit <sahanon.p@ku.th>
2021-10-23 20:36:19 +0700

    add prototype test

--- test-api.py
+++ test-api.py
@@ -1,5 +1,50 @@
-from unittest import TestCase
+import unittest
+import requests
+import datetime
+URL = "https://suchonsite-server.herokuapp.com/"
 
-class TestAPI(TestCase):
+
+class TestAPI(unittest.TestCase):
     def setUp(self):
         pass
+
+    #This test might not necessary but it's a part of the API
+    def test_get_all_people(self):
+        endpoint = URL + "people/all"
+        response = requests.get(endpoint)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual("application/json; charset=utf-8", response.headers["Content-Type"])
+
+    def test_get_today_appionment(self):
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        endpoint = URL + f"people/by_date/{today}"
+        response = requests.get(endpoint)
+        # Today not have any appointment
+        self.assertEqual(response.text, "")
+
+    def test_get_appionment_by_date(self):
+        # Only this date that provide the data
+        endpoint = URL + f"people/by_date/20-10-2021"
+        response = requests.get(endpoint)
+        self.assertEqual("application/json; charset=utf-8", response.headers["Content-Type"])
+
+    def test_get_appiontment_with_unexisting_date(self):
+        endpoint = URL + f"people/by_date/20-20-2021"
+        response = requests.get(endpoint)
+        self.assertEqual(404, response.status_code)
+
+    def test_get_appiontment_without_date(self):
+        endpoint = URL + f"people/by_date/"
+        response = requests.get(endpoint)
+        self.assertEqual(404, response.status_code)
+
+    def test_get_appiontment_with_invalid_date(self):
+        endpoint = URL + f"people/by_date/twenty-oct-2021"
+        response = requests.get(endpoint)
+        self.assertEqual(404, response.status_code)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
