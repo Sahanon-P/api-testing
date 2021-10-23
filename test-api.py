c7ab844b3ef2eca6a7ef3c1c5624da1af8e4d819
Sahanon Phisetpakasit <sahanon.p@ku.th>
2021-10-23 21:27:50 +0700

    fix test name

--- test-api.py
+++ test-api.py
@@ -15,30 +15,30 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual("application/json; charset=utf-8", response.headers["Content-Type"])
 
-    def test_get_today_appionment(self):
+    def test_get_today_appoinment(self):
         today = datetime.datetime.now().strftime("%Y-%m-%d")
         endpoint = URL + f"people/by_date/{today}"
         response = requests.get(endpoint)
         # Today not have any appointment
         self.assertEqual(response.text, "")
 
-    def test_get_appionment_by_date(self):
+    def test_get_appoinment_by_date(self):
         # Only this date that provide the data
         endpoint = URL + f"people/by_date/20-10-2021"
         response = requests.get(endpoint)
         self.assertEqual("application/json; charset=utf-8", response.headers["Content-Type"])
 
-    def test_get_appiontment_with_unexisting_date(self):
+    def test_get_appointment_with_unexisting_date(self):
         endpoint = URL + f"people/by_date/20-20-2021"
         response = requests.get(endpoint)
         self.assertEqual(404, response.status_code)
 
-    def test_get_appiontment_without_date(self):
+    def test_get_appointment_without_date(self):
         endpoint = URL + f"people/by_date/"
         response = requests.get(endpoint)
         self.assertEqual(404, response.status_code)
 
-    def test_get_appiontment_with_invalid_date(self):
+    def test_get_appointment_with_invalid_date(self):
         endpoint = URL + f"people/by_date/twenty-oct-2021"
         response = requests.get(endpoint)
         self.assertEqual(404, response.status_code)
